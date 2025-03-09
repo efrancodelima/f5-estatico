6c8d772fbda25fe6be63d3afc820fde6f61c3ae8
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2025-03-09 16:03:37 -0300

    Atualização periódica da listagem

--- src/app/services/listar-videos/listar-videos.service.ts
+++ src/app/services/listar-videos/listar-videos.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BackEndService } from '../../services/back-end/back-end.service';
 import { Arquivo } from '../../interfaces/arquivo';
+import { Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -25,7 +26,7 @@ export class ListarVideosService {
 
   // Método público
   public listar(): void {
-    const sub = this.backEnd.listarVideos().subscribe({
+    const sub: Subscription = this.backEnd.listarVideos().subscribe({
       next: (resposta: Arquivo[]) => this._processarResposta(resposta),
       error: () => alert('Erro ao listar os vídeos do usuário!'),
       complete: () => sub.unsubscribe(),
@@ -41,6 +42,7 @@ export class ListarVideosService {
     if(resposta.length > 0) {
       this._formatarTimesTamp(resposta);
       this._setArquivos(resposta);
+      this._novaListagem(resposta);
     } else {
       this.resetar();
     }
@@ -52,7 +54,7 @@ export class ListarVideosService {
       const date = new Date(arquivo.timestampInicio);
 
       const dia = String(date.getDate()).padStart(2, '0');
-      const mes = String(date.getMonth() + 1).padStart(2, '0'); // Meses são baseados em zero
+      const mes = String(date.getMonth() + 1).padStart(2, '0');
       const ano = date.getFullYear();
 
       const hora = String(date.getHours()).padStart(2, '0');
@@ -60,6 +62,15 @@ export class ListarVideosService {
       const segundo = String(date.getSeconds()).padStart(2, '0');
 
       arquivo.timestampInicio = `${dia}/${mes}/${ano} ${hora}:${minuto}:${segundo}`;
+    }
+  }
+
+  // Caso haja algum vídeo não finalizado, refaz a consulta após 10 segundos
+  private _novaListagem(arquivos: Arquivo[]): void {
+    for (const arquivo of arquivos) {
+      if (!arquivo.timestampConclusao) {
+        setTimeout(() => this.listar(), 10000);
+      }
     }
   }
 }
