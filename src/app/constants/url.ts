88e99f05e3f3dcde617da58b4de474752b7c62bc
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2025-04-09 11:46:43 -0300

    Atualização da url do API Gateway

--- src/app/constants/url.ts
+++ src/app/constants/url.ts
@@ -1,3 +1,3 @@
-// export const BASE_URL = "http://44.204.140.237:8080"
+export const BASE_URL = "http://44.197.113.122:8080/"
 
-export const BASE_URL = "http://localhost:8080"
+// export const BASE_URL = "http://localhost:8080"
